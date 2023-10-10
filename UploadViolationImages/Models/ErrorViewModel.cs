ccd19f545a72cbd3d0e06dd3f3d038104970a590
imabdallah <imabdallah@USCLDEGYVMP01>
2023-10-10 08:23:33 +0300

    Add project files.

---
+++ UploadViolationImages/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UploadViolationImages.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
